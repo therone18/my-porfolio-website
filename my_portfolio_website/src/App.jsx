e2b9e8e719594368300a3b3df6bdd473e66d629e
TheroneAlmadin <102451950+therone18@users.noreply.github.com>
2023-04-30 12:23:03 +0800

    changes

---
+++ my_portfolio_website/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-[#1C1C1C]  overflow-hidden">
+      <div class="sticky-div">NAV BAR</div>
+      <div id="content" className="w-full overflow-auto">
+      
+      <div className="h-[1024px]">LANDING PAGE</div>
+      <div>ABOUT ME</div>
+      <div>TECH STACK</div>
+      <div>PROJECTS</div>
+      <div>ENDING PAGE</div>
+      
+    </div>
+    </div>
+    
+  );
+};
+
+export default App;
